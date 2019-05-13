105876275784d37b0b0b548363ae99e4fea8eb2b
wahyudi124 <wahyudiprasatia3@gmail.com>
2019-05-13 15:02:44 +0700

    fix controllersssd

--- app/controller/socket.controller.js
+++ app/controller/socket.controller.js
@@ -1,5 +1,5 @@
 const io = require('../../socketio');
 
 io.getIO.on("rectirier_in_data", data =>{
-    io.getIO.in("reactifier_room").emit("rectirier_in_data",data.newValue)
+    io.getIO.in("reactifier_room").emit("rectirier_data",data.newValue)
 }) 
