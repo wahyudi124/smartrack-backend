d462999f0bbc3c7fc200de02c873d29d86589eae
wahyudi124 <wahyudiprasatia3@gmail.com>
2019-05-13 14:55:12 +0700

    fix controllerss

--- app/controller/socket.controller.js
+++ app/controller/socket.controller.js
@@ -1,5 +1,5 @@
 const io = require('../../socketio');
 
 io.getIO.on("rectirier_in_data", data =>{
-    io.getIO.emit("rectirier_in_data",data.newValue)
+    io.getIO.in("reactifier_room").emit("rectirier_in_data",data.newValue)
 }) 
